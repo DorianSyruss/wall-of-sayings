2dc31eb9f3b3c0a98b1307cbaef9a26e9d5a03c9
Ivan Saric <tcd-kakaroto@hotmail.com>
2017-03-31 15:30:29 +0200

    Add basic schemas, models, routes
    - added nodemon for auto server restart
    - added 2 basic routs quotes, users
    - get i post methods for quotes

--- app.js
+++ app.js
@@ -1,24 +1,86 @@
 //require needed dependencies
 const express = require('express');
-const stylus = require('stylus');
-const nib = require('nib');
-const app = express();
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 const bodyParser = require('body-parser');
+const app = express();
+mongoose.connect('mongodb://localhost/test', { config: { autoIndex: false }});
 
-// set view engine to ejs
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib());
-}
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connection established');
+});
 
-// tell node to compile .styl files to normal css files
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
-}));
+/* Define schemas */
+
+const quoteSchema = mongoose.Schema({
+  quote: 'string',
+  author: 'string',
+  favoritedCount: 'Number'
+});
+
+const userSchema = mongoose.Schema({
+  name: 'string',
+  surname: 'string'
+});
+
+/* Define mock-up prototype schema methods */
+
+quoteSchema.methods.getAuthor = function() {
+  return this.author;
+};
+
+userSchema.method.getFullName = function () {
+  return this.name + this.surname;
+};
+
+/* Define models according to schemas */
+
+let Quote = mongoose.model('Quote', quoteSchema);
+let User = mongoose.model('User', userSchema);
+
+
+/* Add some data to db */
+
+User({
+  name: 'Pero',
+  surname: 'Peric'
+}).save();
+
+Quote({
+  quote: 'Whats up',
+  author: 'Ivan S'
+}).save();
+
+/* Routes */
+app.get('/quotes', (req, res) => {
+  Quote.find(function (err, quote) {
+    if (err) return res.send(err);
+    res.send(quote);
+  });
+});
+
+app.post('/quotes', (req, res) => {
+  Quote({
+    quote: req.body.quote,
+    author: 'Ivan S'
+  }).save()
+    .then((data) => {
+    res.send(data);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find(function (err, user) {
+    if (err) return res.send(err);
+    res.send(user);
+  });
+});
 
 app.use(express.static(__dirname + '/public'));
 
